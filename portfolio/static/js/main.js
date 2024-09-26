7c07e4c8b9fbbf92e3fc768beaa99e3d7ccfd0e9
Virendra Singh <vsrathode1109@gmail.com>
2024-09-26 21:47:18 +0530

    Added resume

--- portfolio/static/js/main.js
+++ portfolio/static/js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:['Frontend Developer', 'Python Developer', 'Django Developer'],
+    strings:['Frontend Developer', 'Python Developer', 'Django Developer','Machine Learner Intern'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
